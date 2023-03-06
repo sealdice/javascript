e4411657b351c9aab193e5ee702ea3fba8cbad98
FairyOwO <80889099+FairyOwO@users.noreply.github.com>
2023-03-06 16:23:48 +0800

    Add files via upload

--- scripts/娱乐向/chatgpt/chatgpt_api.py
+++ scripts/娱乐向/chatgpt/chatgpt_api.py
@@ -1,21 +1,57 @@
 from flask import Flask, request
-from revChatGPT.Official import Chatbot
+from revChatGPT.V1 import Chatbot
+import json
+
 app = Flask(__name__)
 
-chatbot = Chatbot(api_key='your api_key',
-                  engine='text-davinci-003')
+
+def getchatgpt(data, chatbot, conversation):
+    full_text = ""
+    for line in chatbot.ask(data, conversation_id=conversation):
+        full_text = line["message"]
+    return full_text
 
 
-def getchatgpt(data, temperature=0.5):
-    global chatbot
-    response = chatbot.ask(data, temperature=temperature)
-    return response["choices"][0]["text"]
-
-@app.route('/')
+@app.route('/', methods=['POST'])
 def chat():
-    # data = json.load(request.data)['text']
-    text = request.args.get('text')
-    return getchatgpt(text)
+    chatbot = None
+    while 1:
+        text = request.json["text"]
+        try:
+            if text == "#reset":
+                chatbot.reset_chat()
+                chatbot.clear_conversations()
+                return "Chat session successfully reset."
+            else:
+                return getchatgpt(text, chatbot, conversatsion)
+        except:
+            if chatbot is None:
+                chatbot = Chatbot(config={
+                        "email": "email",
+                        "password": "your password"
+
+                        # or "session_token": "..."
+                        # cookies on chat.openai.com as "__Secure-next-auth.session-token"
+
+                        # or "access_token": "<access_token>"
+                        # https://chat.openai.com/api/auth/session
+                        }
+                )
+                chatbot.clear_conversations()
+            else:
+                conversatsion = chatbot.get_conversations()
+                chatbot = Chatbot(config={
+                    "email": "email",
+                    "password": "your password"
+
+                    # or "session_token": "..."
+                    # cookies on chat.openai.com as "__Secure-next-auth.session-token"
+
+                    # or "access_token": "<access_token>"
+                    # https://chat.openai.com/api/auth/session
+                }
+                )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5418, debug=False)
