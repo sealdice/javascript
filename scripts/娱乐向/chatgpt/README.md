29b89f48ac88542f7ab02901a037559ec2f0cef4
FairyOwO <80889099+FairyOwO@users.noreply.github.com>
2023-03-08 11:54:13 +0800

    update chatgpt plugin (#20)

--- scripts/娱乐向/chatgpt/README.md
+++ scripts/娱乐向/chatgpt/README.md
@@ -1,12 +1,6 @@
 # usage
- 注册openai chatgpt账户并获得api
- 
- python >= 3.8
- 
- `pip install revChatGPT==1.2.2 openai flask tiktoken`
- 
- `python api.py`
- 
- 具有时效性 随时不能用
- 
- 具有跨群泄露信息的风险 谨慎使用
+## 注册openai chatgpt账户并 ip为中国请用vpn或者代理 推荐使用access_token的登录方式
+## python >= 3.8
+## `pip install revChatGPT==3.1.4 openai==0.26.5 flask tiktoken==0.3.0 OpenAIAuth==0.3.2`
+## `python3 api.py`
+## 具有时效性 随时不能用
--- scripts/娱乐向/chatgpt/chatgpt_api.py
+++ scripts/娱乐向/chatgpt/chatgpt_api.py
@@ -1,21 +1,56 @@
 from flask import Flask, request
-from revChatGPT.Official import Chatbot
+from revChatGPT.V1 import Chatbot
+import json
+
 app = Flask(__name__)
+chatbot = Chatbot(config={
+    'account': 'your_openai_account',
+    'password': 'your_openai_password',
+    # or "session_token": "..."
+    # cookies on chat.openai.com as "__Secure-next-auth.session-token"
 
-chatbot = Chatbot(api_key='your api_key',
-                  engine='text-davinci-003')
+    # or "access_token": "<access_token>"
+    # https://chat.openai.com/api/auth/session
+    'proxy': 'your_proxy',
+}
+)
 
 
-def getchatgpt(data, temperature=0.5):
-    global chatbot
-    response = chatbot.ask(data, temperature=temperature)
-    return response["choices"][0]["text"]
+def getchatgpt(data, chatbot):
+    full_text = ""
+    for line in chatbot.ask(data):
+        full_text = line["message"]
+    return full_text
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def chat():
-    # data = json.load(request.data)['text']
-    text = request.args.get('text')
-    return getchatgpt(text)
+    global chatbot
+    if request.method == 'POST':
+        text = request.json["text"]
+    else:
+        text = request.args.get('text')
+    try:
+        if text == "#reset":
+            chatbot.reset_chat()
+            return "Chat session successfully reset."
+        else:
+            return getchatgpt(text, chatbot)
+    except:
+        chatbot = Chatbot(config={
+            'account': 'your_openai_account',
+            'password': 'your_openai_password',
+
+            # or "session_token": "..."
+            # cookies on chat.openai.com as "__Secure-next-auth.session-token"
+
+            # or "access_token": "<access_token>"
+            # https://chat.openai.com/api/auth/session
+            'proxy': 'your_proxy',
+            'conversation_id ': chatbot.get_conversations[-1]['id']
+        }
+        )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5418, debug=False)
